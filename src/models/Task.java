a7f9573d7c96e0c52c673d55d03da37e27a43229
Josh Robertson <robe0591@algonquinlive.com>
2016-02-08 18:59:25 -0500

    More bean usage

--- src/models/Task.java
+++ src/models/Task.java
@@ -8,6 +8,15 @@ public class Task implements Serializable{
 	 */
 	private static final long serialVersionUID = -218272690039621900L;
 	private String taskName = "Phantasmagorical Photon Shifter";
+	private String taskDescription = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+	
+	public void setTaskDescription(String TaskDescription){
+		this.taskDescription = TaskDescription;
+	}
+	
+	public String getTaskDescription(){
+		return taskDescription;
+	}
 	
 	public void setTaskName(String TaskName){
 		this.taskName = TaskName;
