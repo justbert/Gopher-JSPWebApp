57f0c2adc67ac591daebfd9b5b57ada2f71c97db
justbert <justin.m.bertrand@gmail.com>
2016-02-18 17:56:02 -0500

    Made changes to DBManager -Allows for the creation of a config file.

--- src/utils/DatabaseManager.java
+++ src/utils/DatabaseManager.java
@@ -1,33 +1,72 @@
 package utils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
+
 
 /**
  * 
  * @author Ian
  *
  */
+/*
+ * MODIFIED BY JUSTIN
+ * Made it so that you can keep a local severConfig.txt with the info for connecting to your server.
+ * Make a text file with the following info:
+ * 
+ * url
+ * dbname
+ * driver
+ * username
+ * password
+ * 
+ * Example:
+	jdbc:mysql://localhost:8888/
+	gopher
+	com.mysql.jdbc.Driver
+	GopherAdmin
+	Gophers
+	
+	File locations (for when stuff gets repulled):
+	Justin's file location:
+	C:/Users/Justin/Dropbox/College/Level 5/Web Application Dev/Project/serverConfig.txt
+ */
 public class DatabaseManager {
 	private static final DatabaseManager db = new DatabaseManager(); /* thread-safe singletop instatiation of connection */
 	private Connection conn = null;
      
      private DatabaseManager(){
-    	 String url = "jdbc:mysql://localhost:8888/";  
-         String dbName = "gopher";  
-         String driver = "com.mysql.jdbc.Driver";  
-         String userName = "GopherAdmin";  	/* Temporary Login */
-         String password = "Gophers"; /* Temporary Password*/ 
-         
-         try {
-             Class.forName(driver).newInstance();  /* Reflect database driver */
-             conn = DriverManager.getConnection(url + dbName, userName, password); 
-         } catch (Exception e) {
-             System.out.println(e);
-         }
+    	 
+    	String url, dbName, driver, userName, password;
+    	 
+		try(Scanner input = new Scanner(new FileReader(new File("C:/Users/Justin/Dropbox/College/Level 5/Web Application Dev/Project/serverConfig.txt")))) {
+			url = input.nextLine();  
+			
+			System.out.println("URL: " + url);
+	        dbName = input.nextLine();  
+	        driver = input.nextLine();  
+	        userName = input.nextLine();
+	        password = input.nextLine();
+	         
+	        Class.forName(driver).newInstance();  /* Reflect database driver */
+            conn = DriverManager.getConnection(url + dbName, userName, password); 
+            
+		} catch(FileNotFoundException ex) {
+			System.out.println("File not found: " + ex.getMessage());
+		} catch(IOException ex) {
+			System.out.println("IOException: " + ex.getMessage());
+		} catch(Exception e) {
+			
+		} 
+        
      }
      
      public static DatabaseManager getDatabaseManager(){
