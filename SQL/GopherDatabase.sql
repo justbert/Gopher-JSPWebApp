71b84efb659a2c97590c43d3520e9ab9ea2a64ed
skyeturriff <turriff.skye@gmail.com>
2016-02-23 13:49:47 -0500

    start changes to SQL script

--- SQL/GopherDatabase.sql
+++ SQL/GopherDatabase.sql
@@ -10,109 +10,109 @@ CREATE TABLE IF NOT EXISTS countries (
 
 CREATE TABLE IF NOT EXISTS addresses (
 	id INT unsigned NOT NULL AUTO_INCREMENT,
-	country_id INT unsigned,
+	countryId INT unsigned,
 	city VARCHAR(50),
-	street_address_1 VARCHAR(50),
-	street_address_2 VARCHAR(50),
-	postal_code VARCHAR(6),
-	territory VARCHAR(20),
+	streetAddress1 VARCHAR(50),
+	streetAddress2 VARCHAR(50),
+	postalCode VARCHAR(6),
+	province VARCHAR(20),
 	PRIMARY KEY (id),
-	FOREIGN KEY (country_id)
+	FOREIGN KEY (countryId)
 		REFERENCES countries (id)
 );
 
 CREATE TABLE IF NOT EXISTS users (
     id INT unsigned NOT NULL AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
-	name_first VARCHAR(50),
-	name_last VARCHAR(50),
+	nameFirst VARCHAR(50),
+	nameLast VARCHAR(50),
 	email VARCHAR(50),
-	address_id_home INT unsigned,
-	address_id_work INT unsigned,
-	phone_home VARCHAR(20),
-	phone_mobile VARCHAR(20),
-	phone_work VARCHAR(20),
+	addressIdHome INT unsigned,
+	addressIdWork INT unsigned,
+	phoneHome VARCHAR(20),
+	phoneMobile VARCHAR(20),
+	phoneWork VARCHAR(20),
 	password VARCHAR(50),
-	date_joined TIMESTAMP,
-    is_admin BOOLEAN default false,
+	dateJoined TIMESTAMP,
+    isAdmin BOOLEAN default false,
     PRIMARY KEY (id),
-	FOREIGN KEY (address_id_home)
+	FOREIGN KEY (addressIdHome)
 		REFERENCES addresses (id),
-	FOREIGN KEY (address_id_work)
+	FOREIGN KEY (addressIdWork)
 		REFERENCES addresses (id)
 );
 
 CREATE TABLE IF NOT EXISTS errands (
 	id INT unsigned NOT NULL AUTO_INCREMENT,
-	user_id_customer INT unsigned,
-	user_id_gopher INT unsigned,
-	date_created TIMESTAMP,
-	date_completed TIMESTAMP,
-    reward_id INT unsigned,
+	userIdCustomer INT unsigned,
+	userIdGopher INT unsigned,
+	dateCreated TIMESTAMP,
+	dateCompleted TIMESTAMP,
+    rewardId INT unsigned,
     deadline TIMESTAMP,
     status VARCHAR(200),
 	importance VARCHAR(50),
 	category VARCHAR(50),
 	PRIMARY KEY (id),
-	FOREIGN KEY (user_id_customer)
+	FOREIGN KEY (userIdCustomer)
 		REFERENCES users (id),
-	FOREIGN KEY (user_id_gopher)
+	FOREIGN KEY (userIdGopher)
 		REFERENCES users (id),
-	FOREIGN KEY (rating_id)
+	FOREIGN KEY (rewardId)
 		REFERENCES ratings (id)
 );
 
 CREATE TABLE IF NOT EXISTS rewards(
 	id INT unsigned NOT NULL AUTO_INCREMENT,
     name VARCHAR(100),
-    reward_value numeric(15,2) default NULL,
-    address_id INT unsigned,
+    rewardValue numeric(15,2) default NULL,
+    addressId INT unsigned,
     description VARCHAR(2000),
     PRIMARY KEY (id),
-    FOREIGN KEY (address_id)
+    FOREIGN KEY (addressId)
 		REFERENCES addresses (id)
 );
 
 CREATE TABLE IF NOT EXISTS notifications (
 	id INT unsigned NOT NULL AUTO_INCREMENT,
     title VARCHAR(50),
-    receiving_user_id INT unsigned,
+    userId INT unsigned,
     type INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (receiving_user_id) 
+    FOREIGN KEY (userId) 
     	REFERENCES users(id)
 );
 
 CREATE TABLE IF NOT EXISTS tasks (
 	id INT unsigned NOT NULL AUTO_INCREMENT,
     name VARCHAR(50),
-	errand_id INT unsigned,
-    date_created TIMESTAMP,
-	date_initiated TIMESTAMP,
-	date_completed TIMESTAMP,
-	address_id INT unsigned,
-	is_active boolean,
+	errandId INT unsigned,
+    dateCreated TIMESTAMP,
+	dateInitiated TIMESTAMP,
+	dateCompleted TIMESTAMP,
+	addressId INT unsigned,
+	isActive boolean,
     description VARCHAR(2000),
 	PRIMARY KEY (id),
-	FOREIGN KEY (address_id)
+	FOREIGN KEY (addressId)
 		REFERENCES addresses (id),
-	FOREIGN KEY (errand_id)
+	FOREIGN KEY (errandId)
 		REFERENCES errands (id)
 );
 
 CREATE TABLE IF NOT EXISTS ratings (
 	id INT unsigned NOT NULL AUTO_INCREMENT,
-	user_rated_id INT unsigned,
-	user_rater_id INT unsigned,
-    errand_id INT unsigned,
+	userRatedId INT unsigned,
+	userRaterId INT unsigned,
+    errandId INT unsigned,
 	rating INT,
 	comments VARCHAR(200),
-	date_created TIMESTAMP,
+	dateCreated TIMESTAMP,
 	PRIMARY KEY (id),
-	FOREIGN KEY (user_rated_id)
+	FOREIGN KEY (userRatedId)
 		REFERENCES users (id),
-	FOREIGN KEY (user_rater_id)
+	FOREIGN KEY (userRaterId)
 		REFERENCES users (id),
-	FOREIGN KEY (errand_id) 
+	FOREIGN KEY (errandId) 
 		REFERENCES errands(id)
 );
