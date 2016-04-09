19c377251d1e1d65ad14c182e7cf3d6ea032cb49
peach <iangabriel0@gmail.com>
2016-04-09 15:17:26 -0400

    Revert "Merge branch 'dev' of ssh://gitlab.com/IanGabes/WebDevProject into dev"
    This reverts commit de8f671250ff188ae8b8f12625fd416f8a00b462, reversing
    changes made to e503ac4dfc146c641e83e52f72f2b84b3b5fc724.

---
+++ src/servlets/ErrandServlet.java
@@ -0,0 +1,52 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import daos.ErrandDao;
+import daos.RatingDAO;
+import entities.Errand;
+import entities.Rating;
+
+public class ErrandServlet extends javax.servlet.http.HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private ErrandDao errandDB = new ErrandDao();
+	private RatingDAO ratingDAO = new RatingDAO();
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response){
+
+	}
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response){
+		try {
+			//request.setAttribute("errand", errand);
+			Errand errand = errandDB.selectById((Integer.parseInt(request.getParameter("id"))));
+			System.out.println(errand);
+			request.setAttribute("errand", errand);
+			
+			// Get all ratings for the customer who requested the errand
+			List<Rating> ratings = ratingDAO.getAllCustomerRatingsById(errand.getUserIdCustomer().getId());
+			request.setAttribute("ratingsList", ratings);
+			
+			// Get the average rating for the customer who requested the errand
+			Integer customerRatingAvg = ratingDAO.getRatingAverageForCustomerID(errand.getUserIdCustomer().getId());
+			request.setAttribute("customerAverage", customerRatingAvg);
+			
+			request.getRequestDispatcher("WEB-INF/errand.jsp").forward(request, response);
+		} catch (ServletException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
